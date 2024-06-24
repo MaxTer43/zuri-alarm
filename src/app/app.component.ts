2eccfb513d0eb19b0c5de0728b48f713fb3f397d
MaxTer45 <106483409+MaxTer43@users.noreply.github.com>
2024-06-23 22:59:28 -0500

    fix: vercel json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { environment } from '../environments/environment.prod';
+// @ts-ignore
 import axios from 'axios';
 
 @Component({
